7b633fd4840c5614696646f7230fee56b2efe1f3
Marco Poeta <marcopoeta@MBPr-Marco.local>
2014-09-10 22:06:45 +0200

    first rev of project

--- prova_segue/ViewController.swift
+++ prova_segue/ViewController.swift
@@ -9,7 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-                            
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    @IBOutlet weak var enterNameTextField: UITextField!
+    
+    @IBOutlet weak var enterAMessageTextField: UITextField!
+    
+    @IBOutlet weak var mailButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -17,9 +25,21 @@ class ViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        // Do any additional setup after loading the view, typically from a nib.
     }
+    
 
+    @IBAction func sendMailButtonPressed(sender: UIButton) {
+    // code will evaluate when we press the button.
+        messageLabel.hidden = false
+        messageLabel.text = enterAMessageTextField.text
+        enterAMessageTextField.text = ""
+        enterAMessageTextField.resignFirstResponder() // chiuso la tastiera quando clicchiamo sul bottone.
+        messageLabel.textColor = UIColor.redColor()
+        mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
+        
+    }
+    
 
 }
 
